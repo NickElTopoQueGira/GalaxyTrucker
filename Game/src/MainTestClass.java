ecdd53325ed0ce0cea39beeda3bb3dd38b9fc02a
Shvetsoso <122963299+Shvetsoso@users.noreply.github.com>
2025-03-19 12:06:29 +0100

    test

--- Game/src/MainTestClass.java
+++ Game/src/MainTestClass.java
@@ -4,6 +4,9 @@ public class MainTestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ciao mondo");
+		int c = 7;
+		
+		System.out.println(c);
 
 	}
 
