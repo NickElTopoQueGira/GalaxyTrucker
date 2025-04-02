88b9d3a30863823c45b71ad3675b9c3d04b24e0b
Shvetsoso <122963299+Shvetsoso@users.noreply.github.com>
2025-04-02 16:37:31 +0200

    dettagli
    aggiunta delle new

--- src/carte/Carta.java
+++ src/carte/Carta.java
@@ -14,7 +14,7 @@ public class Carta {
 		
 		int x = 0;
 		Random random = new Random();
-		Carta crt = new Carta(lvl);
+		Carta crt;
 		
 		if(lvl == 3) {
 			x = random.nextInt(12) + 1;
@@ -24,40 +24,40 @@ public class Carta {
 		
 		switch(x) {
 		case 1: // POLVERE_STELLARE,
-			
+			crt = new PolvereStellare();
 		break;
 		case 2: // ZONA_GUERRA,
-			
+			crt = new ZonaGuerra();
 		break;
 		case 3: // PIOGGIA_METEORITI,
-			
+			crt = new PioggiaMeteoriti();
 		break;
 		case 4: // SPAZIO_APERTO,
-		
+			crt = new SpazioAperto();
 		break;
 		case 5: // SCHIAVISTI,
-			
+			crt = new Schiavisti();
 		break;
 		case 6: // CONTRABBANDIERI,
-			
+			crt = new Contrabbandieri();
 		break;
 		case 7: // PIRATI,
-		
+			crt = new Pirati();
 		break;
 		case 8: // STAZIONE_ABBANDONATA,
-			
+			crt = new StazioneAbbandonata();
 		break;
 		case 9: // NAVE_ABBANDONATA,
-			
+			crt = new NaveAbbandonata();
 		break;
 		case 10: // PIANETA,
-		
+			crt = new Pianeta();
 		break;
 		case 11: // EPIDEMIA,
-			
+			crt = new Epidemia();
 		break;
 		case 12: // SABOTAGGIO,
-			
+			crt = new Sabotaggio();
 		break;
 		default:
 			System.out.println("ERROR: creazione carta avventura del mazzo (errorTipe: switch) (class: Carta)");
