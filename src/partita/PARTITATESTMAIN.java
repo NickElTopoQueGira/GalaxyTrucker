f255622d55c737c6420cd2065bc81f686a338c0c
NickElTopoQueGira <48438405+NickElTopoQueGira@users.noreply.github.com>
2025-04-04 11:41:04 +0200

    Main di test per il package partita
    Per fare test sul package partita e per i sub-package

---
+++ src/partita/PARTITATESTMAIN.java
@@ -0,0 +1,35 @@
+package partita;
+
+import partita.giocatore.Colori;
+import partita.giocatore.Giocatore;
+
+import java.util.Scanner;
+
+public class PARTITATESTMAIN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Livelli livelloScelto = Livelli.PRIMO;
+        System.out.println("Seleziona un livello:");
+        for (int i = 0; i < Livelli.values().length; i++) {
+            System.out.println(i + ". " + Livelli.values()[i]);
+        }
+
+        System.out.print("Inserisci il numero del livello: ");
+        int scelta = scanner.nextInt();
+
+        if (scelta >= 0 && scelta < Livelli.values().length) {
+            livelloScelto = Livelli.values()[scelta];
+            System.out.println("Hai selezionato: " + livelloScelto);
+        } else {
+            System.out.println("Scelta non valida!");
+        }
+
+        scanner.close();
+
+        Giocatore g = new Giocatore("Pippo", Colori.ROSSO);
+        g.setLivello(livelloScelto);
+        g.creaNave();
+        
+
+    }
+}
