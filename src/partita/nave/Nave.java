3b8cc18011ea6d20fa67c31a91fd75cd27cea50e
NickElTopoQueGira <48438405+NickElTopoQueGira@users.noreply.github.com>
2025-04-17 21:25:38 +0200

    Aggiunta controlli x inserimento tessera

--- src/partita/nave/Nave.java
+++ src/partita/nave/Nave.java
@@ -7,7 +7,9 @@ import eccezioniPersonalizzate.ErroreTessera;
 import eccezioniPersonalizzate.FinePartita;
 
 import tessera.Coordinate;
+import tessera.LatiTessera;
 import tessera.Tessera;
+import tessera.TipoConnettoriTessera;
 import tessera.TipoTessera;
 
 public abstract class Nave {
@@ -42,9 +44,6 @@ public abstract class Nave {
         // Controllo sulla posizione
         if(i >= 0 && i <= getRighe()){
             if(j >= 0 && j < getColonne()){
-                // controllo se e' collegato a qualche cosa
-                // TODO: agg. i controlli necessari
-
                 // controllo della poszione
 
                 // Verifica se e' nel centro
@@ -60,6 +59,11 @@ public abstract class Nave {
                 // verifca se il pezzo lo si vuole mettere in una posizione gia' occupata
                 if(null == this.nave.get(i).get(j)){
                     throw new ErroreTessera("Posizione gia' occupata");
+                }
+
+                // controllo se e' collegato a qualche cosa
+                if(!controllaCollegamento(tessera, i, j)){
+                    throw new ErroreTessera("La tessera non e' collegata a niente");
                 }
 
                 // verifica se il pezzo e' un motore la sua posizione
@@ -198,6 +202,113 @@ public abstract class Nave {
         }
 
         return true;
+    }
+
+    
+    private boolean controllaCollegamento(Tessera tessera, int i, int k){
+        // true --> il pezzo e' collegato correttamente
+        // false --> il pezzo non e' collegato correttamente
+        /*
+         * Casi controlli
+         *             UP
+         *   SX    ( k - 1 )   DX
+         * (i - 1) |Tessera| (i + 1)
+         *         ( k + 1 )
+         *             DW
+         */
+
+        return controllaCollegamentoSX(tessera, i, k) ||
+                controllaCollegamentoDX(tessera, i, k) ||
+                controllaCollegamentoUP(tessera, i, k) ||
+                controllaCollegamentoDW(tessera, i, k);    
+        }
+
+    public boolean controllaCollegamentoSX(Tessera tessera, int i, int k){
+        if(i - 1 < 0 || null == this.nave.get(i - 1).get(k)){
+            return true;
+        }
+
+        LatiTessera latiTesseraNave = this.nave.get(i - 1).get(k).getLatiTessera();
+
+        // controllo se i lati sono uguali
+        if(latiTesseraNave.getRight() == tessera.getLatiTessera().getLeft()){
+            return true;
+        }
+
+        // controllo sei il ati sono compatibil
+        if((latiTesseraNave.getRight() == TipoConnettoriTessera.TRIPLO) && 
+            (tessera.getLatiTessera().getLeft() != TipoConnettoriTessera.NULLO)){
+                return true;
+            }
+        else{
+            return false;
+        }
+    }
+
+    public boolean controllaCollegamentoDX(Tessera tessera, int i, int k){
+        if(i + 1 > getRighe() || null == this.nave.get(i + 1).get(k)){
+            return true;
+        }
+
+        LatiTessera latiTesseraNave = this.nave.get(i + 1).get(k).getLatiTessera();
+
+        // controllo se i lati sono uguali
+        if(latiTesseraNave.getLeft() == tessera.getLatiTessera().getRight()){
+            return true;
+        }
+
+        // controllo sei il ati sono compatibil
+        if((latiTesseraNave.getLeft() == TipoConnettoriTessera.TRIPLO) && 
+            (tessera.getLatiTessera().getRight() != TipoConnettoriTessera.NULLO)){
+                return true;
+            }
+        else{
+            return false;
+        }
+    }
+
+    public boolean controllaCollegamentoUP(Tessera tessera, int i, int k){
+        if(k - 1 < 0 || null == this.nave.get(i).get(k - 1)){
+            return true;
+        }
+
+        LatiTessera latiTesseraNave = this.nave.get(i).get(k - 1).getLatiTessera();
+
+        // controllo se i lati sono uguali
+        if(latiTesseraNave.getDown() == tessera.getLatiTessera().getUp()){
+            return true;
+        }
+
+        // controllo sei il ati sono compatibil
+        if((latiTesseraNave.getDown() == TipoConnettoriTessera.TRIPLO) && 
+            (tessera.getLatiTessera().getUp() != TipoConnettoriTessera.NULLO)){
+                return true;
+            }
+        else{
+            return false;
+        }
+    }
+
+    public boolean controllaCollegamentoDW(Tessera tessera, int i, int k){
+        if(k + 1 > getColonne() || null == this.nave.get(i).get(k + 1)){
+            return true;
+        }
+
+        LatiTessera latiTesseraNave = this.nave.get(i).get(k + 1).getLatiTessera();
+
+        // controllo se i lati sono uguali
+        if(latiTesseraNave.getUp() == tessera.getLatiTessera().getDown()){
+            return true;
+        }
+
+        // controllo sei il ati sono compatibil
+        if((latiTesseraNave.getUp() == TipoConnettoriTessera.TRIPLO) && 
+            (tessera.getLatiTessera().getDown() != TipoConnettoriTessera.NULLO)){
+                return true;
+            }
+        else{
+            return false;
+        }
     }
 
     public void setCosmonauti(int numeroCosmonauti) { this.numeroCosmonauti = numeroCosmonauti; }
