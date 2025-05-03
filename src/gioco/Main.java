de6f0ff07d065d3f6ac6035bb8c99b736010d342
NickElTopoQueGira <48438405+NickElTopoQueGira@users.noreply.github.com>
2025-05-03 02:39:42 +0200

    zucchero filato

--- src/gioco/Main.java
+++ src/gioco/Main.java
@@ -1,16 +1,14 @@
 package gioco;
 
-import partita.ConfiguraPartita;
-import partita.Partita;
-
 public class Main {
 	public static void main(String[] args){
 		System.out.println("GALAXY TRUCKER\n");
 		
-		ConfiguraPartita conf = new ConfiguraPartita();
-		Partita p = conf.creaPartita();
+		//ConfiguraPartita conf = new ConfiguraPartita();
+		//Partita p = conf.creaPartita();
+		//p.aggiungiGiocatori();
 
-		System.out.println(p);
+		//System.out.println(p);
 		
 	}	
 }
