28dbac39825b8467723b14f07b42f590117a79bf
NickElTopoQueGira <48438405+NickElTopoQueGira@users.noreply.github.com>
2025-05-03 02:35:41 +0200

    classe che incapsula scanner

---
+++ src/partita/Input.java
@@ -0,0 +1,11 @@
+package partita;
+
+import java.util.Scanner;
+
+public class Input {
+    protected final Scanner input;
+
+    public Input(){
+        this.input = new Scanner(System.in);
+    }
+}
