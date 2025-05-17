658a745c714f72882e4a5464dd40b4d95e08c8d4
NickElTopoQueGira <48438405+NickElTopoQueGira@users.noreply.github.com>
2025-05-17 17:55:30 +0200

    Aggiunta nuovo indice per la conversione

--- src/partita/ModalitaPartita.java
+++ src/partita/ModalitaPartita.java
@@ -1,6 +1,23 @@
 package partita;
 
 public enum ModalitaPartita {
-	SINGOLA,
-	MULTIPLA;
+	SINGOLA 	(1),
+	MULTIPLA	(2);
+
+	private final int id;
+
+	private ModalitaPartita(int id){
+		this.id = id;
+	}
+
+	public int getId(){ return this.id; }
+
+	public static ModalitaPartita toModalitaPartita(int mod){
+		for(ModalitaPartita modal : ModalitaPartita.values()){
+			if(modal.getId() == mod){
+				return modal;
+			}
+		}
+		return null;
+	}
 }
