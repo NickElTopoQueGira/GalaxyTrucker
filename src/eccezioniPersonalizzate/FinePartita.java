e29f36470794dccd22c1c7f67d5929ca8bc8c7c8
Shvetsoso <andriy7shvets@gmail.com>
2025-06-12 00:43:07 +0200

    JavaDoc errori

--- src/eccezioniPersonalizzate/FinePartita.java
+++ src/eccezioniPersonalizzate/FinePartita.java
@@ -1,6 +1,11 @@
 package eccezioniPersonalizzate;
 
 public final class FinePartita extends EccezioneGioco{
+	
+	/**
+	 * Metodo Messaggio di errore
+	 * @param msg
+	 */
     public FinePartita(String msg){
         super(msg);
     }
